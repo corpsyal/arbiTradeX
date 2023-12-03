92fed4ef2aa06a4130e80d0a329227885b25d4bc
Anthony Lahlah <lahlah.anthony@gmail.com>
2023-12-03 19:51:05 +0100

    Merge des 2 connexions websockets

--- src/websockets/websockets.module.ts
+++ src/websockets/websockets.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
 import {ExchangeGateIO} from "./exchange-gateIO";
+import {ExchangeBinance} from "./exchange-binance";
 import {combineLatest, merge, switchMap} from "rxjs";
 
 @Module({
-    providers: [ExchangeGateIO]
+    providers: [ExchangeGateIO, ExchangeBinance]
 })
 export class WebsocketsModule {
-    constructor(private _exchangeGateIO: ExchangeGateIO) {
+    constructor(private _exchangeGateIO: ExchangeGateIO, private _exchangeBinance: ExchangeBinance) {
+
         combineLatest([
-            this._exchangeGateIO.isConnected$
+            this._exchangeGateIO.isConnected$,
+            this._exchangeBinance.isConnected$
         ])
             .pipe(switchMap(() => merge(
-                this._exchangeGateIO.messages$
+                this._exchangeGateIO.messages$,
+                this._exchangeBinance.messages$
             )))
             .subscribe()
+
+
     }
 }
